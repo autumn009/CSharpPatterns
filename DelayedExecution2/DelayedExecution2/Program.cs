0b6ee620e6aef0dd32f21f8459accf1e60aa49e3
Akira Kawamata <autumn@piedey.co.jp>
2023-11-06 07:40:21 +0900

    add DelayedExecution2

---
+++ DelayedExecution2/DelayedExecution2/Program.cs
@@ -0,0 +1,18 @@
+﻿Action postProcess;
+switch (DateTime.Now.Second)
+{
+    case 0:
+        Console.WriteLine("前処理1");
+        postProcess = ()=> Console.WriteLine("後処理1");
+        break;
+    case 1:
+        Console.WriteLine("前処理2");
+        postProcess = () => Console.WriteLine("後処理2");
+        break;
+    default:
+        Console.WriteLine("前処理3");
+        postProcess = () => Console.WriteLine("後処理3");
+        break;
+}
+Console.WriteLine("本処理");
+postProcess();

1e828defe8e445424b37981a54891f04e00c46b1
Akira Kawamata <autumn@piedey.co.jp>
2023-11-05 18:37:53 +0900

    add CustomizableMethod1:

---
+++ CustomizableMethod2/CustomizableMethod2/Program.cs
@@ -0,0 +1,20 @@
+﻿var a1 = new StringReader("ABC");
+char[] a2 = { 'A', 'B', 'C' };
+int p = 0;
+
+findA(a1.Read);
+findA(() =>
+{
+    if (p >= a2.Length) return -1;
+    return a2[p++];
+});
+
+void findA(Func<int> getter)
+{
+    for (; ; )
+    {
+        var s = getter();
+        if (s < 0) break;
+        if (s == 'A') Console.WriteLine("Found 'A'");
+    }
+}

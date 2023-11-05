d0b3aef4ac67f01bcc7a1e99ff6ed6101b2db3ae
Akira Kawamata <autumn@piedey.co.jp>
2023-11-06 08:02:48 +0900

    add NullDelegate1

---
+++ NullDelegate1/NullDelegate1/Program.cs
@@ -0,0 +1,8 @@
+﻿Action? postProcess = null;
+if( DateTime.Now.Day == 1)
+{
+    Console.WriteLine("毎月1日のみ前処理を行う");
+    postProcess = () => Console.WriteLine("毎月1日のみ後処理を行う");
+}
+Console.WriteLine("本処理");
+if (postProcess != null) postProcess();

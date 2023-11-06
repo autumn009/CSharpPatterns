2fc2403ea509b186e83b2343dbb0f5db14c3e401
Akira Kawamata <autumn@piedey.co.jp>
2023-11-06 13:45:00 +0900

    add initializeTemplate2

---
+++ initializeTemplate2/initializeTemplate2/Program.cs
@@ -0,0 +1,23 @@
+﻿Person taro = new PersonTemplate() { Name = "タロー", IdNumber = 1 };
+Console.WriteLine($"{taro.Name}={taro.IdNumber}");
+
+class Person
+{
+    internal readonly string? Name;
+    internal readonly int IdNumber;
+    public Person(PersonTemplate t)
+    {
+        Name = t.Name;
+        IdNumber = t.IdNumber;
+    }
+}
+
+class PersonTemplate
+{
+    internal string? Name;
+    internal int IdNumber;
+    public static implicit operator Person(PersonTemplate t)
+    {
+        return new Person(t);
+    }
+}

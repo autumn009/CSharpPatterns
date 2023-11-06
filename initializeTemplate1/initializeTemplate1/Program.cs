18a51b6dd02ea67c55f9ba7a863e3d5e9f161d8f
Akira Kawamata <autumn@piedey.co.jp>
2023-11-06 13:39:10 +0900

    add initializeTemplate1

---
+++ initializeTemplate1/initializeTemplate1/Program.cs
@@ -0,0 +1,8 @@
+﻿var taro = new Person() { Name = "タロー", IdNumber = 1 };
+Console.WriteLine($"{taro.Name}={taro.IdNumber}");
+
+class Person
+{
+    internal string? Name;
+    internal int IdNumber;
+}

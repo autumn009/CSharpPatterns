a0b9c6ef3dd13f810f761a35f57efa34340f6094
Akira Kawamata <autumn@piedey.co.jp>
2023-11-06 20:02:05 +0900

    add ForceToRunStaticConstructor1

---
+++ ForceToRunStaticConstructor1/ForceToRunStaticConstructor1/Program.cs
@@ -0,0 +1,15 @@
+﻿X.ForceToRunConstructor();
+
+static class X
+{
+    internal static void ForceToRunConstructor()
+    {
+        // staticコンストラクタの強制実行用のダミーコード
+        OtherClass.DummyOfDummy = DateTime.Now;
+    }
+    static X() => Console.WriteLine("Running Static Constructor");
+}
+public static class OtherClass
+{
+    public static object? DummyOfDummy { get; set; }
+}

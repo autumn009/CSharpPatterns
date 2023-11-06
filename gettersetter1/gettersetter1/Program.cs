2727692c55c258e2006257f0013d9cd64736ce9d
Akira Kawamata <autumn@piedey.co.jp>
2023-11-06 12:58:40 +0900

    add gettersetter1

---
+++ gettersetter1/gettersetter1/Program.cs
@@ -0,0 +1,10 @@
+﻿int point = 10;
+var dic = new Dictionary<string, int>();
+dic["Taro"] = 10;
+
+sub(ref point);
+Console.WriteLine(point);
+//sub(ref dic["Taro"]);
+//Console.WriteLine(dic["Taro"]);
+
+void sub(ref int p) => p *= 2;

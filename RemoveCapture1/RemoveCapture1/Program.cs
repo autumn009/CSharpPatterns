74cb8c635ad5c6041a0b8a1ba2bf809389e98c20
Akira Kawamata <autumn@piedey.co.jp>
2023-11-06 15:39:13 +0900

    add RemoveCapture1 and 2

---
+++ RemoveCapture1/RemoveCapture1/Program.cs
@@ -0,0 +1,7 @@
+﻿var start = DateTime.Now;
+long sum = 0;
+for (long i = 0; i < 1000000000L; i++) sub(i);
+Console.WriteLine(sum);
+Console.WriteLine(DateTime.Now-start);
+
+void sub(long i) => sum += i;
---
+++ RemoveCapture2/RemoveCapture2/Program.cs
@@ -0,0 +1,10 @@
+﻿var start = DateTime.Now;
+Console.WriteLine(sub());
+Console.WriteLine(DateTime.Now - start);
+
+static long sub()
+{
+    long sum = 0;
+    for (long i = 0; i < 1000000000L; i++) sum += i;
+    return sum;
+}

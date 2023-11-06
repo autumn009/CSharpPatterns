68c44507a787cfc91198a8f51f65a0da9742cfab
Akira Kawamata <autumn@piedey.co.jp>
2023-11-06 15:25:04 +0900

    add PrivateToLocal1

---
+++ PrivateToLocal1/PrivateToLocal1/Program.cs
@@ -0,0 +1,16 @@
+﻿var x = new X();
+x.Sub();
+
+class X
+{
+    private int calc(int x, int y) => (int)Math.Sqrt(x * x + y * y);
+    internal void Sub()
+    {
+        Console.WriteLine(calc(1, 2));
+        Console.WriteLine(calc(3, 4));
+    }
+    internal void Sub2()
+    {
+        // このメソッドからcalcメソッドを呼び出すことができてしまうが適切ではない。
+    }
+}

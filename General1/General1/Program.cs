67b21780cf2cf32b5387d6ef4847b104a72611a2
Akira Kawamata <autumn@piedey.co.jp>
2023-11-06 11:50:10 +0900

    General1

---
+++ General1/General1/Program.cs
@@ -0,0 +1,22 @@
+﻿int[] array = { 123, 456, 789 };
+Console.WriteLine(General.RandomSelect(array));
+General.Dump(array);
+
+static class General
+{
+    internal static T RandomSelect<T>(IEnumerable<T> source)
+    {
+        var index = Random.Shared.Next(source.Count());
+        return source.ElementAt(index);
+    }
+    internal static void Dump<T>(IEnumerable<T> source)
+    {
+        Console.Write("{ ");
+        foreach (var item in source.Select((n, i) => (n, i)))
+        {
+            if (item.i > 0) Console.Write(", ");
+            Console.Write(item.n);
+        }
+        Console.WriteLine(" }");
+    }
+}

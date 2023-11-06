666a30d585422a6efebf7fa887f337fa4387822b
Akira Kawamata <autumn@piedey.co.jp>
2023-11-06 13:06:46 +0900

    gettersetter2

---
+++ gettersetter2/gettersetter2/Program.cs
@@ -0,0 +1,10 @@
+﻿int point = 10;
+var dic = new Dictionary<string, int>();
+dic["Taro"] = 10;
+
+sub((t) => point = t, () => point);
+Console.WriteLine(point);
+sub((t)=> dic["Taro"]=t,()=> dic["Taro"]);
+Console.WriteLine(dic["Taro"]);
+
+void sub(Action<int> setter, Func<int> getter) => setter(getter() * 2);

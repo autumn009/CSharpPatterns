9d7ed718df5661f5deada7780d4cf7b227d5d4e0
Akira Kawamata <autumn@piedey.co.jp>
2023-11-07 19:34:24 +0900

    add ToList1/2

---
+++ ToList1/ToList1/Program.cs
@@ -0,0 +1,9 @@
+﻿int[] array = { 1, 2, 3 };
+List<int> list = new();
+list.AddRange(array);
+sub(list);
+
+void sub(List<int> list)
+{
+    foreach (int i in list) Console.WriteLine(i);
+}
---
+++ ToList2/ToList2/Program.cs
@@ -0,0 +1,7 @@
+﻿int[] array = { 1, 2, 3 };
+sub(array.ToList());
+
+void sub(List<int> list)
+{
+    foreach (int i in list) Console.WriteLine(i);
+}

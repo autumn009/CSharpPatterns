5e3e6d0e636006ad6f4aa83e5e18302586a47f71
Akira Kawamata <autumn@piedey.co.jp>
2023-11-07 09:28:51 +0900

    rewrite it

--- RemoveCapture1/RemoveCapture1/Program.cs
+++ RemoveCapture1/RemoveCapture1/Program.cs
@@ -1,7 +1,10 @@
 ﻿var start = DateTime.Now;
 long sum = 0;
-for (long i = 0; i < 1000000000L; i++) sub(i);
+sub();
 Console.WriteLine(sum);
-Console.WriteLine(DateTime.Now-start);
+Console.WriteLine(DateTime.Now - start);
 
-void sub(long i) => sum += i;
+void sub()
+{
+    for (long i = 0; i < 1000000000L; i++) sum += i;
+}

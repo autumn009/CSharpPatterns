48695c477fbfa68af88bcc106686ce69dddf8723
Akira Kawamata <autumn@piedey.co.jp>
2023-11-07 14:40:15 +0900

    add range1/2

---
+++ range1/range1/Program.cs
@@ -0,0 +1,8 @@
+﻿int count = 0;
+for (int i = 2084; ; i += 4)
+{
+    if (i % 100 == 0 && i % 400 != 0) continue;
+    Console.WriteLine(i);
+    count++;
+    if (count >= 10) break;
+}
---
+++ range2/range2/Program.cs
@@ -0,0 +1,4 @@
+﻿foreach (var item in Enumerable.Range(0, int.MaxValue).Select(c => 2084 + c * 4).Where(c => c % 100 != 0 || c % 400 == 0).Take(10))
+{
+    Console.WriteLine(item);
+}

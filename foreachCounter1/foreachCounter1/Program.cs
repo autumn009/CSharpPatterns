e729a6b066c233f42aafd1dc0cec0cff0c89d73c
Akira Kawamata <autumn@piedey.co.jp>
2023-11-07 14:11:21 +0900

    add foreachCounter

---
+++ foreachCounter1/foreachCounter1/Program.cs
@@ -0,0 +1,5 @@
+﻿int[] src = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+for (int i = 3; i < src.Length; i+=2)
+{
+    Console.WriteLine(src[i]);
+}
---
+++ foreachCounter2/foreachCounter2/Program.cs
@@ -0,0 +1,5 @@
+﻿int[] src = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+foreach (var item in src.Skip(3).Select((n,i)=>(n,i)).Where(c=>c.i % 2 == 0))
+{
+    Console.WriteLine(item.n);
+}

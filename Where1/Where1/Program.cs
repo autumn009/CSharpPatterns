8f2820cb40ea6810c6cd5cb84f6b76103a705377
Akira Kawamata <autumn@piedey.co.jp>
2023-11-08 15:00:16 +0900

    add Where1/2

---
+++ Where1/Where1/Program.cs
@@ -0,0 +1,9 @@
+﻿string[] src = { "Alice", "Becky", "Cindy", "Adam" };
+
+foreach (var item in src)
+{
+    if (item.StartsWith("A"))
+    {
+        Console.WriteLine(item);
+    }
+}
---
+++ Where2/Where2/Program.cs
@@ -0,0 +1,6 @@
+﻿string[] src = { "Alice", "Becky", "Cindy", "Adam" };
+
+foreach(var item in src.Where(c=>c.StartsWith("A")))
+{
+    Console.WriteLine(item);
+}

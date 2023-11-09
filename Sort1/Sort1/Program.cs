603f727d3d3ae6ae86719355453bea94adadb914
Akira Kawamata <autumn@piedey.co.jp>
2023-11-09 09:21:16 +0900

    add Sort1/2

---
+++ Sort1/Sort1/Program.cs
@@ -0,0 +1,7 @@
+﻿int[] array = { 1, 3, 2 };
+var dup = array.ToArray();
+Array.Sort(dup);
+foreach (var item in dup)
+{
+    Console.WriteLine(item);
+}
---
+++ Sort2/Sort2/Program.cs
@@ -0,0 +1,5 @@
+﻿int[] array = { 1, 3, 2 };
+foreach (var item in array.Order())
+{
+    Console.WriteLine(item);
+}

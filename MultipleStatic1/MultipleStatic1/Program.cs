49685493e0f236f4a6d18249e74b8f45e6669153
Akira Kawamata <autumn@piedey.co.jp>
2023-11-09 13:52:31 +0900

    add MultipleStatic1

---
+++ MultipleStatic1/MultipleStatic1/Program.cs
@@ -0,0 +1,10 @@
+﻿Sample<int>.Value = 1;
+Sample<long>.Value = 2;
+Console.WriteLine(Sample<int>.Value);
+Console.WriteLine(Sample<long>.Value);
+
+static class Sample<T>
+{
+    internal static int Value { get; set; }
+}
+```

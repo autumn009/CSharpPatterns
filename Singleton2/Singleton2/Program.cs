4fd35debaf206771214fe7306b006682ad1489dc
Akira Kawamata <autumn@piedey.co.jp>
2023-11-09 13:21:04 +0900

    add Singleton2

---
+++ Singleton2/Singleton2/Program.cs
@@ -0,0 +1,9 @@
+﻿Sample.OnlySample.OnlyOneValue = 123;
+Console.WriteLine(Sample.OnlySample.OnlyOneValue);
+
+class Sample
+{
+    internal int OnlyOneValue { get; set; } = 0;
+    internal static Sample OnlySample = new Sample();
+    private Sample() { }
+}

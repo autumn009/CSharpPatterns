0260367aaa0b1cd752c75a1df23eb619de0151fe
Akira Kawamata <autumn@piedey.co.jp>
2023-11-09 12:50:18 +0900

    add Random1

---
+++ Random1/Random1/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("予測不能の乱数");
+for (int i = 0; i < 5; i++) sub(new Random());
+Console.WriteLine("再現性のある乱数");
+const int seed = 123456;
+for (int i = 0; i < 5; i++) sub(new Random(seed));
+
+void sub(Random random)
+{
+    for (int i = 0; i < 10; i++)
+    Console.Write($"{random.Next(10)} ");
+    Console.WriteLine();
+}

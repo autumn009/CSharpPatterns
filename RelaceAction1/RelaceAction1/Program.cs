f5f21b0255bef306df855a85f1835d975012478d
Akira Kawamata <autumn@piedey.co.jp>
2023-11-09 11:36:16 +0900

    add ReplaceAction1

---
+++ RelaceAction1/RelaceAction1/Program.cs
@@ -0,0 +1,26 @@
+﻿string[] src =
+{
+    "Waiting",
+    "NORMAL",
+    "Start",
+    "SINGLE",
+    "Hello!",
+    "How are you?",
+    "DOUBLE",
+    "Let's go!",
+    "NORMAL",
+    "THE END"
+};
+Action<string> action = (s)=> { };
+
+foreach (var line in src)
+{
+    if (line == "NORMAL") setNormal();
+    else if (line == "SINGLE") setSingle();
+    else if (line == "DOUBLE") setDouble();
+    else action(line);
+}
+
+void setNormal() => action = (s) => Console.WriteLine(s);
+void setSingle() => action = (s) => Console.WriteLine($"'{s}'");
+void setDouble() => action = (s) => Console.WriteLine($"\"{s}\"");

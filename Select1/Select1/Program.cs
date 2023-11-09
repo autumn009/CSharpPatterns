d905a75ae9c903c2b9e77e688e6498bbfd07a383
Akira Kawamata <autumn@piedey.co.jp>
2023-11-09 09:04:16 +0900

    add Select1/2

---
+++ Select1/Select1/Program.cs
@@ -0,0 +1,13 @@
+﻿var dic = new Dictionary<int, string>();
+dic.Add(1,"TARO");
+dic.Add(2, "JIRO");
+dic.Add(3, "TAMAKO");
+int[] ids = { 1, 2 };
+foreach (var item in ids)
+{
+    var name = dic[item];
+    if (name.StartsWith("T"))
+    {
+        Console.WriteLine(name);
+    }
+}
---
+++ Select2/Select2/Program.cs
@@ -0,0 +1,9 @@
+﻿var dic = new Dictionary<int, string>();
+dic.Add(1, "TARO");
+dic.Add(2, "JIRO");
+dic.Add(3, "TAMAKO");
+int[] ids = { 1, 2 };
+foreach (var name in ids.Select(c => dic[c]).Where(c => c.StartsWith("T")))
+{
+    Console.WriteLine(name);
+}

74e9aaa936b9ba80dbbed70ff26cc2d0d3a6b209
Akira Kawamata <autumn@piedey.co.jp>
2023-11-09 09:50:36 +0900

    add WalkTypes1

---
+++ WalkTypes1/WalkTypes1/Program.cs
@@ -0,0 +1,22 @@
+﻿foreach (var assembly in AppDomain.CurrentDomain.GetAssemblies())
+{
+    foreach (var item in assembly.GetTypes())
+    {
+        if (item.GetCustomAttributes(typeof(MyTargetAttribute), true).Length > 0)
+        {
+            dynamic? obj = Activator.CreateInstance(item);
+            obj?.Sub();
+        }
+    }
+}
+
+class MyTargetAttribute : Attribute { }
+
+[MyTarget]
+class MyClass
+{
+    public void Sub()
+    {
+        Console.WriteLine("Sub called");
+    }
+}

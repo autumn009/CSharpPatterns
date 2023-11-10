1166568536417170f48e0d9733f86af0c5a4c10b
Akira Kawamata <autumn@piedey.co.jp>
2023-11-10 15:49:51 +0900

    add Ractpry1/2

---
+++ Factory1/Factory1/Program.cs
@@ -0,0 +1,36 @@
+﻿sub(true);
+sub(false);
+
+void sub(bool IsMale)
+{
+    Character? c;
+    if (IsMale)
+        c = new Male { Name = "オルテガの息子" };
+    else
+        c = new Female { Name = "オルテガの娘" };
+    c.Say();
+}
+
+abstract class Character
+{
+    internal string Name { get; set; } = "";
+    internal abstract void Say();
+}
+
+class Male : Character
+{
+    internal override void Say()
+    {
+        Console.WriteLine($"オレは{Name}だぜ!");
+    }
+}
+
+class Female : Character
+{
+    internal override void Say()
+    {
+        Console.WriteLine($"私は{Name}よ!");
+    }
+}
+
+
---
+++ Factory2/Factory2/Program.cs
@@ -0,0 +1,39 @@
+﻿sub(true);
+sub(false);
+
+void sub(bool IsMale)
+{
+    var c = Character.CreateCharacter(IsMale);
+    c.Say();
+}
+
+abstract class Character
+{
+    internal string Name { get; set; } = "";
+    internal abstract void Say();
+    internal static Character CreateCharacter(bool IsMale)
+    {
+        Character? c;
+        if (IsMale)
+            c = new Male { Name = "オルテガの息子" };
+        else
+            c = new Female { Name = "オルテガの娘" };
+        return c;
+    }
+
+    private class Male : Character
+    {
+        internal override void Say()
+        {
+            Console.WriteLine($"オレは{Name}だぜ!");
+        }
+    }
+
+    private class Female : Character
+    {
+        internal override void Say()
+        {
+            Console.WriteLine($"私は{Name}よ!");
+        }
+    }
+}

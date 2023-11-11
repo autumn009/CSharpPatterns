e94bddf80c1002a4f2a07bb445b8d81225c587ff
Akira Kawamata <autumn@piedey.co.jp>
2023-11-11 14:16:16 +0900

    add PrimaryConstructor1/2

---
+++ PrimaryConstructor1/PrimaryConstructor1/Program.cs
@@ -0,0 +1,8 @@
+﻿var obj = new Person("Taro");
+Console.WriteLine(obj.Name);
+
+class Person
+{
+    public string Name { get; init; }
+    public Person(string name) => Name = name;
+}
---
+++ PrimaryConstructor2/PrimaryConstructor2/Program.cs
@@ -0,0 +1,7 @@
+﻿var obj = new Person("Taro");
+Console.WriteLine(obj.Name);
+
+class Person(string name)
+{
+    public string Name { get; init; } = name;
+}

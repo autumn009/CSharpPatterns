3f471f61facc1375fc3b1e68ae53a35160605a94
Akira Kawamata <autumn@piedey.co.jp>
2023-11-11 14:01:47 +0900

    add Record1/2

---
+++ Record1/Record1/Program.cs
@@ -0,0 +1,9 @@
+﻿var obj = new Immutable("Taro");
+//obj.Name = "Jiro"; // 不変だから書けない
+Console.WriteLine(obj.Name);
+
+class Immutable
+{
+    public string Name { get; init; }
+    public Immutable(string name) => Name = name;
+}
---
+++ Record2/Record2/Program.cs
@@ -0,0 +1,5 @@
+﻿var obj = new Immutable("Taro");
+//obj.Name = "Jiro"; // 不変だから書けない
+Console.WriteLine(obj.Name);
+
+record Immutable(string Name);
